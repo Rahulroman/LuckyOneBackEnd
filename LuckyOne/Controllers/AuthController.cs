a20a72107c8e287147fb59243761ccc8ca824320
rahulroman <rahulshinde2144@gmail.com>
2025-11-25 22:39:34 +0530

    Add project files.

---
+++ LuckyOne/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuckyOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> login()
+        {
+            var result = "data test";
+            return  Ok(result);
+        }
+    }
+}
